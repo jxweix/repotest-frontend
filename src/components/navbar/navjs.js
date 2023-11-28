536c51553f5a2764c0c903bcff05ef811823265e
save <jpjpjp.phone10@gmail.com>
2023-11-29 05:48:33 +0700

    fix navbar | link path

--- src/components/navbar/navjs.js
+++ src/components/navbar/navjs.js
@@ -1,30 +1,49 @@
 import React from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Input, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar} from "@nextui-org/react";
-import {AcmeLogo} from "./AcmeLogo.jsx";
-import {SearchIcon} from "./SearchIcon.jsx";
+import { Button, Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Input, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar } from "@nextui-org/react";
+import { AcmeLogo } from "./AcmeLogo.jsx";
+import { SearchIcon } from "./SearchIcon.jsx";
+import Image from "next/image.js";
+import Iconmsg from "../../../public/icons/Iconmsg.png";
+import Iconnoti from "../../../public/icons/Iconnoti.png";
+import { usePathname } from "next/navigation.js";
 
 export default function App() {
+  const current = usePathname();
+
   return (
-    <Navbar className="Navbar" isBordered>
+    <Navbar Navbar maxWidth={'full'} className="Navbar" isBordered>
       <NavbarContent justify="start">
         <NavbarBrand className="mr-4">
           <AcmeLogo />
           <p className="hidden sm:block font-bold text-inherit">ACME</p>
         </NavbarBrand>
         <NavbarContent className="hidden sm:flex gap-3">
-          <NavbarItem>
-            <Link color="foreground" href="#">
-              Features
+          <NavbarItem
+            isActive={current == '/test'}
+          >
+            <Link
+              color="foreground" href="./test"
+              className={current == '/test' ? 'active-item' : ''}>
+              หน้าแรก
             </Link>
           </NavbarItem>
-          <NavbarItem isActive>
-            <Link href="#" aria-current="page" color="secondary">
-              Customers
+          <NavbarItem
+            isActive={current == '/addboard'}
+          >
+            <Link
+              color="foreground" href="./addboard" aria
+              className={current == '/addboard' ? 'active-item' : ''}>
+              สร้างบอร์ด
             </Link>
           </NavbarItem>
-          <NavbarItem>
-            <Link color="foreground" href="#">
-              Integrations
+          <NavbarItem
+            isActive={current == '/search'}
+          >
+            <Link
+              color="foreground" href="./search"
+              className={current == '/search' ? 'active-item' : ''}
+            >
+              หน้าค้นหา
             </Link>
           </NavbarItem>
         </NavbarContent>
@@ -33,16 +52,25 @@ export default function App() {
       <NavbarContent as="div" className="items-center" justify="end">
         <Input
           classNames={{
-            base: "max-w-full sm:max-w-[10rem] h-10",
+            base: "max-w-full h-10",
             mainWrapper: "h-full",
-            input: "text-small",
-            inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+            input: ["text-small", "bg-transparent", "text-white"],
+            inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 border-white dark:bg-transparent-500/20",
+
           }}
           placeholder="Type to search..."
           size="sm"
+          radius="full"
+          variant="bordered"
           startContent={<SearchIcon size={18} />}
           type="search"
         />
+        <Button variant="light" isIconOnly >
+          <Image src={Iconnoti} width={28} />
+        </Button>
+        <Button variant="light" isIconOnly >
+          <Image src={Iconmsg} width={28} />
+        </Button>
         <Dropdown placement="bottom-end">
           <DropdownTrigger>
             <Avatar
