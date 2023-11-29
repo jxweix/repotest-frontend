6b9d5b772562c4e6bcc41f898c68f0fcc9e2321b
Awk <Natthanon4869@gmail.com>
2023-11-29 22:07:28 +0700

    globals | cardphoto

---
+++ src/pages/api/cardphoto.js
@@ -0,0 +1,31 @@
+export default [
+    {
+        img: "/photo/pic-1.jpg",
+        title: "hello",
+        text: "1",
+    },
+    {
+        img: "/photo/pic-1.jpg",
+        title: "what",
+        text: "2",
+    },
+    {
+        img: "/photo/pic-1.jpg",
+        title: "ta",
+        text: "3",
+    },
+    {
+        img: "/photo/pic-1.jpg",
+        title: "fak",
+        text: "4",
+    },
+    {
+        img: "/photo/pic-1.jpg",
+        title: "ka",
+        text: "5",
+    },
+    {
+        img: "/photo/pic-1.jpg",
+        title: "bro",
+        text: "6",    }
+]
