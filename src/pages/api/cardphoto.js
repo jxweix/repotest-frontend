fe60208d9a925315cf90b036de7b249461825e67
Mean <Krisada.nyt@gmail.com>
2023-11-29 23:37:55 +0700

    feat : train map

--- src/pages/api/cardphoto.js
+++ src/pages/api/cardphoto.js
@@ -1,31 +1,36 @@
-export default [
-    {
-        img: "/photo/pic-1.jpg",
-        title: "hello",
-        text: "1",
-    },
-    {
-        img: "/photo/pic-1.jpg",
-        title: "what",
-        text: "2",
-    },
-    {
-        img: "/photo/pic-1.jpg",
-        title: "ta",
-        text: "3",
-    },
-    {
-        img: "/photo/pic-1.jpg",
-        title: "fak",
-        text: "4",
-    },
-    {
-        img: "/photo/pic-1.jpg",
-        title: "ka",
-        text: "5",
-    },
-    {
-        img: "/photo/pic-1.jpg",
-        title: "bro",
-        text: "6",    }
-]
+const mockData = [
+  {
+    img: "/photo/pic-1.jpg",
+    title: "hello",
+    text: "1",
+  },
+  {
+    img: "/photo/pic-1.jpg",
+    title: "what",
+    text: "2",
+  },
+  {
+    img: "/photo/pic-1.jpg",
+    title: "ta",
+    text: "3",
+  },
+  {
+    img: "/photo/pic-1.jpg",
+    title: "fak",
+    text: "4",
+  },
+  {
+    img: "/photo/pic-1.jpg",
+    title: "ka",
+    text: "5",
+  },
+  {
+    img: "/photo/pic-1.jpg",
+    title: "bro",
+    text: "6",
+  },
+];
+
+export default function handler(req, res) {
+  res.status(200).json({ mock: mockData });
+}
