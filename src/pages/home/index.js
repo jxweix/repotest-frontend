8a5cf02c85016e2edd8e6fc89ef9752c953319a9
Awk <Natthanon4869@gmail.com>
2023-12-02 16:32:25 +0700

    fix repo | text size

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,14 +6,16 @@ function home() {
         <div className="BG-page123">
             <div className="wrapper">
                 <div className="grid grid-rows-2 justify-items-center">
-                    <p className=" grid row-span-1 text-[64px] text-center text-white font-semibold">มาใช้ประโยชน์จากเวลาว่างกันเถอะ</p>
+                    <p className="grid row-span-1 text-[36px] md:text-[64px] text-center text-white font-semibold">
+                        หาอะไรทำอยู่ใช่ไหม? มาสิ
+                    </p>
                     <Button
-                        className="w-[160px] h-[50px] text-white bg-gradient-to-r from-purple-950 to-violet-300 via-violet-700 to-transparent shadow"
+                        className="w-80px md:w-[160px] h-25px md:h-[50px] text-white bg-gradient-to-r from-purple-950 to-violet-300 via-violet-700 to-transparent shadow"
                         radius="full"
-                        variant='light'
+                        variant="light"
                         color="secondary"
                     >
-                        <p className='text-[24px]'>สร้างบัญชี</p>
+                        <p className="text-[18px] md:text-[24px]">สร้างบัญชี</p>
                     </Button>
                 </div>
             </div>
