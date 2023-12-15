79af17880d92338ffc8823b692dcc7d6ecad141f
jxweix <jpjpjp.phone10@gmail.com>
2023-12-15 07:02:54 +0700

    change protocol

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -7,18 +7,18 @@ import type { NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url)
+  const protocol = request.headers.get("x-forwarded-proto") || "http";
+  console.log("🚀 ~ file: route.ts:11 ~ GET ~ protocol:", protocol)
+  console.log("🚀 ~ file: route.ts:10 ~ GET ~ requestUrl:", requestUrl)
   const code = requestUrl.searchParams.get('code')
 
   if (code) {
     const cookieStore = cookies()
     const supabase = createRouteHandlerClient<Database>({ cookies: () => cookieStore })
-    const response = await supabase.auth.exchangeCodeForSession(code)
+    await supabase.auth.exchangeCodeForSession(code)
 
-    if (response.error) {
-      return NextResponse.redirect(`${requestUrl.origin}/allboard`)
-    }
-  // URL to redirect to after sign in process completes
-    return NextResponse.redirect(`${requestUrl.origin}/board`)
+    // URL to redirect to after sign in process completes
+    // return NextResponse.redirect(`${requestUrl.origin}/board`)
+    return NextResponse.redirect(`${protocol}://${requestUrl.host}/board`)
   }
-  return NextResponse.redirect(`${requestUrl.origin}/`)
 }
