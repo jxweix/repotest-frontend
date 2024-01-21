0d4498f8cd8192e119c0f4f7dcbbce3a71ebc5da
coluxuz <omecurry7913@gmail.com>
2024-01-21 15:12:56 +0700

    fetch board data and sec

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,52 +1,104 @@
-"use client"
-import './style.css'
-import React, { useEffect, useState } from 'react'
-import Image from 'next/image'
-import { Card, CardHeader } from '@nextui-org/react';
+"use client";
+import "./style.css";
+import React, { use, useEffect, useState } from "react";
+import Image from "next/image";
+import { Card, CardHeader } from "@nextui-org/react";
 import { Database } from "@App/types/database.types";
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import {
+  User,
+  createClientComponentClient,
+} from "@supabase/auth-helpers-nextjs";
+import { keys } from "@mantine/core";
+import { json } from "stream/consumers";
 
-function boardHome() {
-  const [res, setRes] = useState<any>([]);
+function BoardHome() {
+  const [user, setUser] = useState<User | null>(null);
+  const [boardData, setBoardData] = useState<any>([]);
+  const [srcData, setSrcData] = useState<any>([]);
+  const [codata, setCodata] = useState<any>([]);
+
   const supabase = createClientComponentClient<Database>();
-
   useEffect(() => {
     const fetchData = async () => {
+      const { data } = await supabase.auth.getUser();
+      setUser(data.user);
+      console.log("user ID is:", data.user?.id);
+
       try {
-        let { data: testCreate } = await supabase
-          .from('testCreate')
-          .select('src');
-        if (testCreate) {
-          setRes(testCreate)
+        // Fetch data from joinBoardtbl
+        const { data: joinData } = await supabase
+          .from("release_joinboard")
+          .select("*")
+          .eq("keyUser", data.user?.id);
+
+        if (joinData) {
+          // Fetch data from release_boardtbl
+          const { data: boardData } = await supabase
+            .from("release_boardtbl")
+            .select("*");
+
+          // Extract keyImage values from boardData
+          const keyImages = boardData?.map((board) => board.keyImage);
+
+          if (keyImages) {
+            // Fetch data from release_type_img where keyImage is in keyImages
+            const { data: srcData } = await supabase
+              .from("release_type_img")
+              .select("*")
+              .in("keyImage", keyImages);
+
+            console.log("board data : ", boardData);
+            console.log("src data : ", srcData);
+
+            const joinData = boardData?.map((item1) => {
+              const match = srcData?.find(
+                (item2) => item2.keyImage === item1.keyImage
+              );
+              return { ...item1, ...match };
+            });
+            if (joinData) {
+              setCodata(joinData);
+              console.log("join data : ", joinData);
+            }
+          }
         }
-
       } catch (error: any) {
-        console.error('Error fetching data:', error?.message);
+        console.error("Error fetching data:", error?.message);
       }
-    }; fetchData();
-  }, []);
+    };
 
-  const comimage = res?.map((img:any) => (
-    <Card className='figure' key={img.key} isFooterBlurred>
-      <CardHeader className="Header backdrop-blur">
-        <div className='Item-hidden backdrop-contrast-50'>
-          <h4 className="mix-blend-screen font-medium text-[24px]">ชื่อบอร์ด</h4>
-          <p className="text-[16px] uppercase font-bold">สถานที่ map</p>
-        </div>
-      </CardHeader>
-      <Image className='imgcard' src={img.src} alt='photo' width={590} height={590} />
-    </Card >
-  ));
+    fetchData();
+  }, [boardData, srcData]);
 
   return (
     <div>
-      <div className='wrapper'>
-        <div className='container'>
-          {comimage}
+      <div className="wrapper">
+        <div className="container">
+          {codata?.map((data: any, i: number) => (
+            <Card className="figure" key={i} isFooterBlurred>
+              <CardHeader className="Header backdrop-blur">
+                <div className="Item-hidden backdrop-contrast-50">
+                  <h4 className="mix-blend-screen font-medium text-[20px]">
+                    {data.nameBoard}
+                  </h4>
+                  <p className="text-[16px] uppercase font-bold">
+                    {data.detail}
+                  </p>
+                </div>
+              </CardHeader>
+              <Image
+                className="imgcard"
+                src={data.src}
+                alt="photo"
+                width={590}
+                height={590}
+              />
+            </Card>
+          ))}
         </div>
       </div>
     </div>
   );
 }
 
-export default boardHome;
+export default BoardHome;
