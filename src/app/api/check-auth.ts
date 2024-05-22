cf460ce290cc6ca367c63852c59a3c962607a34b
jxweix <jpjpjp.phone10@gmail.com>
2024-05-22 14:11:57 +0700

    cheng js

--- src/app/api/check-auth.ts
+++ src/app/api/check-auth.ts
@@ -1,6 +1,7 @@
 import { getSession } from 'next-auth/client';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const session = await getSession({ req });
 
   if (session) {
